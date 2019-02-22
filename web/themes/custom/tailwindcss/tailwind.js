2540df58583df3274d4fd9f893abbe81b5e5125d
Arif Khan <arifkhan.incaendo@gmail.com>
2019-02-22 08:02:09 +0530

    Cart icon changes

--- web/themes/custom/tailwindcss/tailwind.js
+++ web/themes/custom/tailwindcss/tailwind.js
@@ -20,6 +20,16 @@ const colors = {
   // 'drupal-blue': '#0678be',
 }
 
+const fonts = {
+  'open-sans': [
+    'Open Sans',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+  ],
+  ...defaultConfig.fonts
+}
+
 module.exports = {
   ...defaultConfig,
 
@@ -29,6 +39,62 @@ module.exports = {
   backgroundColors: colors,
   borderColors: Object.assign({ default: colors['grey-light'] }, colors),
 
+  // Override Fonts
+  fonts:fonts,
+
+  
+  modules: {
+    appearance: ['responsive'],
+    backgroundAttachment: ['responsive'],
+    backgroundColors: ['responsive', 'hover', 'focus'],
+    backgroundPosition: ['responsive'],
+    backgroundRepeat: ['responsive'],
+    backgroundSize: ['responsive'],
+    borderCollapse: [],
+    borderColors: ['responsive', 'hover', 'focus'],
+    borderRadius: ['responsive'],
+    borderStyle: ['responsive'],
+    borderWidths: ['responsive'],
+    cursor: ['responsive'],
+    display: ['responsive'],
+    flexbox: ['responsive'],
+    float: ['responsive'],
+    fonts: ['responsive'],
+    fontWeights: ['responsive', 'hover', 'focus'],
+    height: ['responsive'],
+    leading: ['responsive'],
+    lists: ['responsive'],
+    margin: ['responsive'],
+    maxHeight: ['responsive'],
+    maxWidth: ['responsive'],
+    minHeight: ['responsive'],
+    minWidth: ['responsive'],
+    negativeMargin: ['responsive'],
+    objectFit: false,
+    objectPosition: false,
+    opacity: ['responsive'],
+    outline: ['focus'],
+    overflow: ['responsive'],
+    padding: ['responsive'],
+    pointerEvents: ['responsive'],
+    position: ['responsive'],
+    resize: ['responsive'],
+    shadows: ['responsive', 'hover', 'focus'],
+    svgFill: [],
+    svgStroke: [],
+    tableLayout: ['responsive'],
+    textAlign: ['responsive'],
+    textColors: ['responsive', 'hover', 'focus'],
+    textSizes: ['responsive'],
+    textStyle: ['responsive', 'hover', 'focus'],
+    tracking: ['responsive'],
+    userSelect: ['responsive'],
+    verticalAlign: ['responsive'],
+    visibility: ['responsive'],
+    whitespace: ['responsive'],
+    width: ['responsive'],
+    zIndex: ['responsive']
+  },
   // Override any options.
   options: {
     ...defaultConfig.options,
